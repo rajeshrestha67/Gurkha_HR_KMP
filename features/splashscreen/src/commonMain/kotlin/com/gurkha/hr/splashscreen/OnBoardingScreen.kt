b1474292f7bfcbbf87dfda3478649e9e2243802f
sunilshrestha07 <sunilcrestha9@gmail.com>
2025-09-06 12:10:41 +0545

    feat: completed onboarding screen

--- features/splashscreen/src/commonMain/kotlin/com/gurkha/hr/splashscreen/OnBoardingScreen.kt
+++ features/splashscreen/src/commonMain/kotlin/com/gurkha/hr/splashscreen/OnBoardingScreen.kt
@@ -18,10 +18,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -60,7 +60,7 @@ fun OnBoardingScreen(
                     Text("")
                 },
                 actions = {
-                    IconButton(
+                    TextButton(
                         modifier = Modifier.padding(MaterialTheme.dimens.small2),
                         onClick = {
 //                            not fixed
