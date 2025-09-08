69b202de4ba047ad23d0a0e030f489444aeaa35c
chirag.dangol <chirag@ambition.guru>
2025-09-08 11:25:03 +0545

    feat: log removed

--- core/networkHelper/src/commonMain/kotlin/com/gurkha/hr/networkhelper/safeCall.kt
+++ core/networkHelper/src/commonMain/kotlin/com/gurkha/hr/networkhelper/safeCall.kt
@@ -57,7 +57,6 @@ suspend inline fun <reified T> responseToResult(
         413 -> ERPResult.Error(DataError.NetworkError.PayloadTooLarge)
         in 500..599 -> ERPResult.Error(DataError.NetworkError.Server)
         else -> {
-            println("status ${T::class}, ${LoginResponseDto::class}, ${T::class == LoginResponseDto::class}")
             if (T::class == LoginResponseDto::class) {
                 val res = try {
                     response.body<ErrorData>()
