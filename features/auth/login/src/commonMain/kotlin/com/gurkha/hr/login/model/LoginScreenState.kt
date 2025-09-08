e6a6313143deedd5c928a97150ed6683a1240bcb
chirag.dangol <chirag@ambition.guru>
2025-09-08 16:45:52 +0545

    feat: hardcore value removed

--- features/auth/login/src/commonMain/kotlin/com/gurkha/hr/login/model/LoginScreenState.kt
+++ features/auth/login/src/commonMain/kotlin/com/gurkha/hr/login/model/LoginScreenState.kt
@@ -3,8 +3,8 @@ package com.gurkha.hr.login.model
 import org.jetbrains.compose.resources.StringResource
 
 data class LoginScreenState(
-    val username: String = "Chirag.dangol@mbank.com.np",
-    val password: String = "OVf#9PfTs",
+    val username: String = "",
+    val password: String = "",
     val usernameError: StringResource? = null,
     val passwordError: StringResource? = null,
     val isLoading: Boolean = false,
