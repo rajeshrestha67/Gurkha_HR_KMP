cabc38a44f7d6f91108d1080d47b6ef3aff1c956
chirag.dangol <chirag@ambition.guru>
2025-09-08 14:48:49 +0545

    feat: iOS app icon and splash screen added

--- composeApp/src/iosMain/kotlin/com/gurkha/hr/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/gurkha/hr/MainViewController.kt
@@ -1,5 +1,33 @@
 package com.gurkha.hr
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
+import com.gurkha.hr.splashscreen.OnBoardingViewModel
+import com.gurkha.hr.splashscreen.model.OnBoardingAction
+import org.koin.compose.viewmodel.koinViewModel
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    var showSplashScreen by remember { mutableStateOf(true) }
+    var navigateToOnBoarding by remember { mutableStateOf(true) }
+    val viewModel: OnBoardingViewModel = koinViewModel()
+    LaunchedEffect(Unit) {
+        viewModel.action(OnBoardingAction.CheckFirstUser)
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.navigationChannel.collect { isFirstTime ->
+            showSplashScreen = false
+            navigateToOnBoarding = isFirstTime
+        }
+    }
+
+    if (!showSplashScreen) {
+        App(
+            isFirstTime = navigateToOnBoarding
+        )
+    }
+}
