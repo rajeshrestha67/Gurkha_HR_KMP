e451e6a2f3c3a2b874b9d14f3d1fb0f99fcfaf62
sunilshrestha07 <sunilcrestha9@gmail.com>
2025-09-17 14:14:42 +0545

    feat: data updated

--- core/model/src/commonMain/kotlin/com/gurkha/model/attendance/attendanceDto.kt
+++ core/model/src/commonMain/kotlin/com/gurkha/model/attendance/attendanceDto.kt
@@ -7,24 +7,53 @@ data class AttendanceRequestDto(
     val fromDate: String,
     val toDate: String,
     val enableManualAttendance: String,
-    val branchId : String
+    val branchId: String
 )
 
 @Serializable
 data class AttendanceResponseDto(
-   val status: String? = null,
+    val status: String? = null,
     val message: String? = null,
-
-
-)
+    val detail: List<AttendanceDetailResponse> ?= null,
+    val success: Boolean? = null
+    )
 
 @Serializable
 data class AttendanceDetailResponse(
-    val data : HashMap<String, AttendanceDataResponse>? = null
-
+    val data: HashMap<String, AttendanceDataResponse>? = null
 )
+
 @Serializable
 data class AttendanceDataResponse(
-    val data : HashMap<String, Nothing>? = null
-
+    val attendanceId: Int? = null,
+    val created: String? = null,
+    val forRequestedDate: String? = null,
+    val employeeId: Int? = null,
+    val employeeName: String? = null,
+    val employeeType: String? = null,
+    val departmentId: Int? = null,
+    val branchId: Int? = null,
+    val branchName: String? = null,
+    val clockInTime: String? = null,
+    val clockOutTime: String? = null,
+    val clockInImage: String? = null,
+    val clockOutImage: String? = null,
+    val employeeMapId: String? = null,
+    val onLeave: Boolean? = null,
+    val attendanceStatus: String? = null,
+    val enableManualAttendance: String? = null,
+    val enableImageAttendance: String? = null,
+    val attendanceRequestStatus: String? = null,
+    val isLeaveModification: String? = null,
+    val isLeaveEarly: Boolean? = null,
+    val isLate: Boolean? = null,
+    val message: String? = null,
+    val dayOfWeek: String? = null,
+    val reason: String? = null,
+    val workingHrs: String? = null,
+    val imageUrl: String? = null,
+    val leaveDuration: String? = null,
+    val lateInDuration: String? = null,
+    val approvedRemarks: String? = null,
+    val holiday: Boolean? = null
 )
