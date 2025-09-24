2fdc3347b0c60ae1ec51571d590465a46a139d90
chirag.dangol <chirag.dangol@mbank.com.np>
2025-09-24 21:23:47 +0545

    feat: crypto ios fixed

--- core/crypto/src/androidMain/kotlin/com/gurkha/hr/crypto/CryptoFactory.android.kt
+++ core/crypto/src/androidMain/kotlin/com/gurkha/hr/crypto/CryptoFactory.android.kt
@@ -6,14 +6,12 @@ actual object CryptoFactory {
     actual suspend inline fun <reified T> encrypt(t: T): ByteArray? {
         val str = Json.encodeToString(t)
         val bytes = str.encodeToByteArray()
-
-        return bytes//str.encodeToByteArray()//Crypto.safeEncrypt(bytes)
+        return Crypto.safeEncrypt(bytes)
     }
 
     actual suspend inline fun <reified T> decrypt(bytes: ByteArray): T? {
-//        val decryptedBytes = Crypto.safeDecrypt(bytes)
-        val json = bytes.decodeToString()
-        val data: T = json.let { Json.decodeFromString(it) }
-        return data
+        val decryptedBytes = Crypto.safeDecrypt(bytes)
+        val json = decryptedBytes?.decodeToString()
+        return json?.let { Json.decodeFromString(it) }
     }
 }
