78cc981d3e01ba4053bb6097b6626be5ea3b50b1
chirag.dangol <chirag.dangol@mbank.com.np>
2025-10-14 17:41:49 +0545

    feat : unused variable removed

--- features/home/src/commonMain/kotlin/com/gurkha/hr/home/model/HomeScreenState.kt
+++ features/home/src/commonMain/kotlin/com/gurkha/hr/home/model/HomeScreenState.kt
@@ -11,7 +11,6 @@ import com.gurkha.hr.date.data.CalendarDay
 import com.gurkha.hr.domain.attendance.attendanceReport.model.AttendanceData
 import com.gurkha.hr.domain.upComingBirthday.model.UpComingBirthdayData
 import com.gurkha.hr.domain.upComingWorkAnniversaries.model.UpComingWorkAnniversaryData
-import com.gurkha.hr.domain.userDetail.model.UserDetailData
 
 data class HomeScreenState(
 
@@ -30,20 +29,11 @@ data class HomeScreenState(
     val isAttendanceLoading: Boolean = false,
     val isBirthDayLoading: Boolean = false,
     val isAnniversaryLoading: Boolean = false,
-    val fromDate: String = "2025-09-16",
-    val toDate: String = "2025-09-17",
-    val enableManualAttendance: String = "N",
-    val branchId: String? = null,
 
     val attendanceReport: List<AttendanceData>? = null,
-    val userDetail: UserDetailData? = null,
     val upComingBirthday: List<UpComingBirthdayData> = emptyList(),
     val upComingWorkAnniversary: List<UpComingWorkAnniversaryData> = emptyList(),
-
-    val clockInTime: String = "",
-    val clockOutTime: String = "",
-    val isLeaveEarly: String = "",
-    val isLate: String = "",
+    
     val calendarData: List<CalendarDay> = listOf(),
     val todayBS: CalendarDate,
     val selectedDay: Int = 1
