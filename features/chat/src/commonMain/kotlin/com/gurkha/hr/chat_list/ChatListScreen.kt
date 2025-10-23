fd7ce75d4973aa73575421d655dbbe0a6d273b25
chirag.dangol <chirag.dangol@mbank.com.np>
2025-10-23 22:18:10 +0545

    feat : imePadding added

--- features/chat/src/commonMain/kotlin/com/gurkha/hr/chat_list/ChatListScreen.kt
+++ features/chat/src/commonMain/kotlin/com/gurkha/hr/chat_list/ChatListScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -85,7 +86,7 @@ private fun ChatListScreenContent(
     onAction: (ChatListScreenAction) -> Unit
 ) {
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().imePadding(),
         containerColor = MaterialTheme.colorScheme.background,
         topBar = {
             TopAppBar(
