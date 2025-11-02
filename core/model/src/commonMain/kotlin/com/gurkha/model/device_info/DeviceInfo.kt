14efa5c7f8053a9a67720dee83cccd56e8e98ced
chirag.dangol <chirag.dangol@mbank.com.np>
2025-11-02 14:40:27 +0545

    feat : device info abstraction

---
+++ core/model/src/commonMain/kotlin/com/gurkha/model/device_info/DeviceInfo.kt
@@ -0,0 +1,18 @@
+package com.gurkha.model.device_info
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DeviceInfo(
+    val platform: String,
+    val manufacturer: String?,
+    val model: String?,
+    val osVersion: String?,
+    val sdkInt: String?,
+    val locale: String?,
+    val timezone: String?,
+    val appVersion: String?,
+    val appBuild: String?,
+    val deviceName: String?,
+    val isEmulator: Boolean
+)
